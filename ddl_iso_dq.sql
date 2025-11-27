505cc663ecca258ed29ec0c18b74ea1f6f959994
sidbas <sidbasha@gmail.com>
2025-11-27 13:55:27 +0000

    Update ddl_iso_dq.sql

--- ddl_iso_dq.sql
+++ ddl_iso_dq.sql
@@ -14,6 +14,62 @@ BEGIN
                JSON_OBJECT(
                     'path'      VALUE r.path,
                     'required'  VALUE r.required,
+                    'exists'    VALUE EXISTSNode(
+                                    XMLTYPE(p_xml_msg),
+                                    CASE 
+                                        WHEN SUBSTR(r.path,1,1)='/' 
+                                          THEN r.path
+                                        ELSE '/'||r.path
+                                    END
+                                 ),
+                    'valid'     VALUE 
+                        CASE 
+                            WHEN r.required = 1 
+                             AND EXISTSNode(
+                                    XMLTYPE(p_xml_msg),
+                                    CASE 
+                                        WHEN SUBSTR(r.path,1,1)='/' 
+                                          THEN r.path
+                                        ELSE '/'||r.path
+                                    END
+                                ) = 0
+                                THEN 'missing'
+                            ELSE 'ok'
+                        END
+               )
+           RETURNING CLOB)
+    INTO l_result
+    FROM JSON_TABLE(
+        l_rules_json,
+        '$.rules[*]'
+        COLUMNS (
+            path      VARCHAR2(400) PATH '$.path',
+            required  NUMBER        PATH '$.required'
+        )
+    ) r;
+
+    RETURN l_result;
+END;
+/
+
+
+
+CREATE OR REPLACE FUNCTION validate_iso_message (
+    p_xml_msg  IN CLOB,
+    p_xsd_name IN VARCHAR2
+) RETURN CLOB
+AS
+    l_rules_json CLOB;
+    l_result     CLOB;
+BEGIN
+    SELECT rule_json INTO l_rules_json
+    FROM iso_dq_rules
+    WHERE xsd_name = p_xsd_name;
+
+    SELECT JSON_ARRAYAGG(
+               JSON_OBJECT(
+                    'path'      VALUE r.path,
+                    'required'  VALUE r.required,
                     'exists'    VALUE EXISTSNode(XMLTYPE(p_xml_msg), r.path),
                     'valid'     VALUE 
                         CASE 
